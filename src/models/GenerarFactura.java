a77a2dc2416d2492f9621828497d88218186e51e
joelgarciaarevalo <joelgarciaarevalo.dev@gmail.com>
2025-06-04 08:50:34 -0500

    agregar pom y .jar

---
+++ src/models/GenerarFactura.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class GenerarFactura {
+    
+}
